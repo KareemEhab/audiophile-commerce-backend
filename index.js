8fa30cd01e5561fb235c0a42fecf5c361f8b01a4
KareemEhab <kareem8002010@gmail.com>
2024-03-27 20:59:06 +0200

    Cors issue try 4

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
 require("express-async-errors");
 const logger = require("./services/logging");
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+app.use(
+  cors({
+    origin: "https://audiophile-ecommerce-webapp.vercel.app",
+  })
+);
 
 require("./startup/routes")(app);
 require("./startup/db")();
