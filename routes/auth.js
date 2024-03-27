abc2dac5e2ea22f4aeba03380f2c15bdc0e193bf
KareemEhab <kareem8002010@gmail.com>
2024-03-27 22:50:08 +0200

    Try 13

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,7 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/", async (req, res) => {
+  console.log(res);
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
